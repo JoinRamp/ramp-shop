436c798059d8a34c47910f116b2ae910ac6bee3d
AdroitCode <kamponistullar@gmail.com>
2024-07-29 16:00:32 +0100

    fix cached data :fire:

--- src/components/product/card.tsx
+++ src/components/product/card.tsx
@@ -52,24 +52,6 @@ export default function Card({ product }: { product: Product }) {
           onClick={() => openModal('PRODUCT_DETAILS', { slug })}
           className="absolute left-0 top-0 z-10 flex h-full w-full cursor-pointer items-center justify-center gap-9 bg-dark/60 p-4 opacity-0 backdrop-blur-sm transition-all group-hover:gap-5 group-hover:opacity-100 dark:bg-dark/70"
         >
-          {/* <button
-            className={cn(
-              'text-center font-medium text-light',
-              isGridCompact ? 'text-xs' : 'text-13px'
-            )}
-          >
-            <div
-              className={cn(
-                'mb-2 flex items-center justify-center rounded-full bg-dark-800 text-light backdrop-blur-sm transition-all hover:bg-brand',
-                isGridCompact ? 'h-11 w-11' : 'h-[50px] w-[50px]'
-              )}
-            >
-              <PreviewIcon
-                className={cn(isGridCompact ? 'h-4 w-4' : 'h-5 w-5')}
-              />
-            </div>
-            {t('text-preview')}
-          </button> */}
           <button
             onClick={goToContactUsPage}
             className={cn(
